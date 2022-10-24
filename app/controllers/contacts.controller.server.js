24cc7959a6f17889f7803582bdae0c1349a533b0
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-24 19:27:20 -0400

    Display Add Contacts page completed

--- app/controllers/contacts.controller.server.js
+++ app/controllers/contacts.controller.server.js
@@ -7,7 +7,75 @@ export function DisplayContactsList(req, res, next){
             res.end(err);
         }
 
-        res.render('index', { title: 'Contact List', page: 'contacts/list', contacts: contactsCollection  }); // later add ---displayName: UserDisplayName(req)
+        res.render('index', { title: 'Contact List', page: 'contacts/list', contacts: contactsCollection  }); // later add ---displayName: UserDisplayName(req)  to all
 
     })
 }
+export function DisplayContactsAddPage(req, res, next){
+    res.render('index', { title: 'Add Contact', page: 'contacts/edit', bcontact: {} });
+}
+ 
+export function ProcessContactsAddPage(req, res, next){
+    let newBContact = contactsModel({
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email
+        
+    });
+
+    contactsModel.create(newBContact, (err, bcontact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/list')
+    })
+}
+
+export function DisplayContactsEditPage(req, res, next){
+    let id = req.params.id;
+
+    contactsModel.findById(id, (err, bcontact) => {
+        if(err){
+            console.error(err);
+            res.ens(err);
+        }
+
+        res.render('index', { title: 'Edit Contacts', page: 'contacts/edit', bcontact: bcontact });
+    })
+   
+}
+export function ProcessContactsEditPage(req, res, next){
+
+    let id = req.params.id;
+    let newBContact = contactsModel({
+        _id: req.body.id,
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email
+        
+    });
+
+    contactsModel.updateOne( {_id: id}, newBContact, (err, bcontact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/list')
+    })
+}
+export function ProcessContactsDelete(req, res, next){
+    let id = req.params.id;
+
+    contactsModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/list');
+    })
+}
+
--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
-import { DisplayContactsList} from "../controllers/contacts.controller.server.js";
+import { DisplayContactsList, DisplayContactsAddPage, ProcessContactsAddPage, ProcessContactsEditPage, DisplayContactsEditPage, ProcessContactsDelete} from "../controllers/contacts.controller.server.js";
 
 const router = Router();
 
 router.get('/list', DisplayContactsList);
+router.get('/contacts-add',  DisplayContactsAddPage);     //add authguard later
+router.post('/contacts-add',  ProcessContactsAddPage);
+router.post('/contacts-edit/:id', ProcessContactsEditPage);
+router.get('/contacts-edit/:id',  DisplayContactsEditPage);
+router.get('/contacts-delete/:id', ProcessContactsDelete); 
 
 export default router;
