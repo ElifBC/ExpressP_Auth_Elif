579a5af4b45fd815b9f49350bb43e6ddde751b1a
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-24 22:04:23 -0400

    Testing Atlas Connection

--- config/config.js
+++ config/config.js
@@ -3,4 +3,4 @@ export const Secret = 'My Secret';
 //export const MongoURI = "mongodb://127.0.0.1:27017/media" // mongo connection URL
 
 //Configuation of database connection
-export const MongoURI = "mongodb+srv://ElifBC:DartyZigot@mongocluster1.xxdcmfh.mongodb.net/media1?retryWrites=true&w=majority"
+export const MongoURI = "mongodb+srv://ElifBC:DartyZigot@mongocluster1.xxdcmfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
