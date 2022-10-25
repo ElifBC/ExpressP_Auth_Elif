2815ca01a200ebf881fb18437176ac04263d64fe
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-24 21:03:08 -0400

    Mongo Atlas setup complted

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
 export const Secret = 'My Secret';
+
+//export const MongoURI = "mongodb://127.0.0.1:27017/media" // mongo connection URL
+
 //Configuation of database connection
-export const MongoURI = "mongodb://127.0.0.1:27017/media" // mongo connection URL
-//export const MongoURI = "mongodb+srv://ElifBC:DartyZigot@mongocluster1.xxdcmfh.mongodb.net/media1?retryWrites=true&w=majority"
+export const MongoURI = "mongodb+srv://ElifBC:DartyZigot@mongocluster1.xxdcmfh.mongodb.net/media1?retryWrites=true&w=majority"
